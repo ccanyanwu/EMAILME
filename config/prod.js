904a38dfbd8601b54c683a4389fdcc7efc9d1d92
ccanyanwu <eme42c@gmail.com>
2021-04-27 19:48:02 +0100

    production db

--- config/prod.js
+++ config/prod.js
@@ -1,16 +1,16 @@
 //production keys
-/* module.exports = {
+module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
-}; */
+};
 
-module.exports = {
+/* module.exports = {
   googleClientID:
     "857955165608-52b1ml7vndnl4vjcb17pt8e9qdn1k6nd.apps.googleusercontent.com",
   googleClientSecret: "1mfyBZGMmfDaAk_LNENrtbpg",
   mongoURI:
     "mongodb+srv://FRESH:FRESH@fresh.t1l4i.mongodb.net/FRESH?retryWrites=true&w=majority",
   cookieKey: "tg83uiynftj4uvklfh vlgjgbnjbhngjbng",
-};
+}; */
